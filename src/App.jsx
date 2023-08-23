586d9c1aa26af089c11cee6d9e75dd8bbfd634e1
Tanmoy Adhikary <adhikary.tanmoy@gmail.com>
2023-08-24 00:45:21 +0530

    updated the logo path

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
+import logo from "./assets/images/logog.png";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <nav>
-          <ul>
-            <li>
+      <header className="header">
+        <nav id="nav">
+          <ul className="nav-list">
+            <li className="nav-list__items">
               <Link to="/">
-                <img src="src\assets\images\logog.png" alt="" />
+                <img src={logo} alt="vanlife logo" className="logo-img" />
               </Link>
             </li>
-            <li>
+            <li className="nav-list__items">
               <Link to="/about">About</Link>
             </li>
-            <li>
+            <li className="nav-list__items">
               <Link to="/vans">Vans</Link>
             </li>
           </ul>
